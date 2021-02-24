e16652d8ec0b83d93b7cdd32887122c7e3679f48
Jared Suresky <jsuresky@gmail.com>
2021-02-23 20:44:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jchat
+ FSA Stackathon Project
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const config = require("./config/app");
+const app = express();
+
+require("dotenv").config();
+
+app.get("/home", (req, res) => {
+  return res.send("Home Screen");
+});
+
+app.get("/login", (req, res) => {
+  return res.send("Login Screen");
+});
+
+const port = config.appPort;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
